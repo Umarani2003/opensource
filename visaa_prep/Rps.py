2300fb507525e1c88cf427b31afe1cd0e4a3674a
Umarani2003 <160024143+Umarani2003@users.noreply.github.com>
2024-11-18 12:14:38 +0530

    Create Rps.py

---
+++ visaa_prep/Rps.py
@@ -0,0 +1,23 @@
+m,n = input().split()
+if m == 'R' :
+    if n == 'p':
+        print("Charan")
+    elif n == 'S' :
+        print("Vignesh")
+    else:
+        print("NULL")
+elif m == "P" :
+    if n == 'S' :
+        print("Charan")
+    elif n == 'R' :
+        print("Vignesh")
+    else:
+         print("NULL")
+elif m == 'S' :
+    if n == 'R' :
+        print("Charan")
+    elif n == 'P' :
+        print("Vignesh")
+    else:
+        print("NULL")
+              
